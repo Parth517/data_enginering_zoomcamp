a4bb1c3a8f5b491ed109bd9666888efddc327958
Parth <50496941+Parth517@users.noreply.github.com>
2026-01-18 07:00:49 +0000

    moved_to_docker_terraform_folder

---
+++ module_1_docker_and_terraform/pipeline/ingest_data.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+import click
+
+prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+   
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+df = pd.read_csv(
+    prefix + '/yellow_tripdata_2021-01.csv.gz',
+    dtype=dtype,
+    nrows=100,
+    parse_dates=parse_dates
+)
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
+    year=2021
+    month=1
+
+    # pg_user ='root'
+    # pg_password ='root'
+    # pg_host ='localhost'
+    # pg_port =5432
+    # pg_db ='ny_taxi'
+    # chunksize=100000
+    # target_table='yellow_taxi_data'
+    url=f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+
+    
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
+    )
+    
+    
+    first=True
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(n=0).to_sql(
+                name=target_table, 
+                con=engine, 
+                if_exists='replace'
+            )
+            first=False
+        df_chunk.to_sql(
+            name=target_table, 
+            con=engine, 
+            if_exists='append'
+        )
+
+
+if __name__ == '__main__':
+    run()
+
+
